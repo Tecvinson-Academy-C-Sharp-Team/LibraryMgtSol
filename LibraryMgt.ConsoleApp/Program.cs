f17e578ce1efc575bc0b69662c9cdee04e0987aa
Godswill David <davidgodswill@gmail.com>
2024-07-07 12:43:19 +0100

    user management service

---
+++ LibraryMgt.ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using LibraryMgt.Core.Entities;
+using LibraryMgt.Service.Services;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Create a user");
+User user = new User()
+{
+    Id = 1,
+    Name = "Godswill David",
+    Email = "godswill.david@tecvinsonacademy.com",
+    IsAdmin = true,
+    IsActive = true
+};
+
+UserService userService = new UserService(new LibraryService());
+string userMessage = "New user created";
+
+userService.CreateUser(user);
+
+Console.WriteLine(userMessage);
---
+++ LibraryMgt.Service/Services/UserService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryMgt.Core.Entities;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace LibraryMgt.Service.Services
+{
+    public class UserService
+    {
+        public List<User> users = new List<User>();
+        private readonly LibraryService _libraryService;
+
+        public UserService(LibraryService libraryService)
+        {
+            _libraryService = libraryService;
+        }
+
+        // Create CRUD
+        // Create
+        // Read
+        // Update
+        // Delete
+        public void CreateUser(User user)
+        {
+            // add a new user to the book db
+            users.Add(user);
+        }
+
+        public User GetUser(int id)
+        {
+            var user = users.FirstOrDefault(x => x.Id == id);
+            return user;
+        }
+
+        public List<User> GetUsers()
+        {
+            return users;
+        }
+
+        public Book BorrowBook(string title, User user)
+        {
+            if (string.IsNullOrWhiteSpace(title))
+                throw new ArgumentNullException("The name of the book is required!");
+
+            if (user is not null)
+                throw new ArgumentNullException("The user must be a valid user!");
+
+            var book = _libraryService.GetBooks().FirstOrDefault(b => b.Title == title);
+            if (book == null && book.IsLocked)
+                throw new ArgumentNullException("The book you want borrow is not available right now.");
+
+            book.IsLocked = true;
+            _libraryService.UpdateBook(book);
+            return book;
+        }
+
+        public void ReturnBorrow(Book book)
+        {
+            if (book is null)
+                throw new ArgumentNullException("You must provide a book!");
+
+            book.IsLocked = false;
+
+            _libraryService.UpdateBook(book);
+        }
+    }
+}
